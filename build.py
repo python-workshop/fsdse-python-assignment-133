1b507bc0727a47f647c7bdd984d1edeae1bfe25a
unknown <jaymewadada@live.com>
2017-05-15 23:57:43 +0530

    done

---
+++ build.py
@@ -0,0 +1,53 @@
+from collections import deque
+
+
+class Node:
+    def __init__(self, info):
+        self.info = info
+        self.left = None
+        self.right = None
+        self.level = None
+
+    def __str__(self):
+        return str(self.info)
+
+
+def BFT(node):
+    node.level = 1
+    queue = deque([node])
+    output = []
+    current_level = node.level
+
+    while len(queue) > 0:
+
+        current_node = queue.popleft()
+
+        if (current_node.level > current_level):
+            output.append("\n")
+            current_level += 1
+
+        output.append(str(current_node))
+
+        if current_node.left != None:
+            current_node.left.level = current_level + 1
+            queue.append(current_node.left)
+
+        if current_node.right != None:
+            current_node.right.level = current_level + 1
+            queue.append(current_node.right)
+
+    return ''.join(output)
+
+
+root = Node(9)
+
+root.left = Node(2)
+root.right = Node(10)
+
+root.left.left = Node(1)
+root.left.right = Node(3)
+
+root.right.left = Node(5)
+root.right.right = Node(11)
+
+print BFT(root)
